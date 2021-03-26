056a0f7c6e2a5d71d6254ef694f3d7810117e2e7
Julius <yulii.vedkal@nure.ua>
2021-03-26 22:03:46 +0200

    Add project files.

---
+++ TDDTestSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDDTestSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
