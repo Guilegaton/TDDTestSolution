6d146a9e4576533dae494befc9fe9a849ead9257
Julius <yulii.vedkal@nure.ua>
2021-03-26 23:45:25 +0200

    [add] - lcd digits unit tests

---
+++ TDDTestSolution.Tests/LCDDigitsServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TDDTestSolution.Services;
+
+namespace TDDTestSolution.Tests
+{
+    public class LCDDigitsServiceTests
+    {
+        [Test]
+        public void Convert_Correct()
+        {
+            // Arrange
+            var service = new LCDDigitsService();
+            uint value = 15;
+            var expectedValue =
+@"... ._.
+..| | _.
+..| ._ |";
+
+            // Act
+            var result = service.Convert(value);
+
+            // Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+    }
+}
---
+++ TDDTestSolution/Services/LCDDigitsService.cs
@@ -0,0 +1,12 @@
+﻿using TDDTestSolution.Interfaces;
+
+namespace TDDTestSolution.Services
+{
+    public class LCDDigitsService : ILCDDigitsService
+    {
+        public string Convert(uint value)
+        {
+            return string.Empty;
+        }
+    }
+}
